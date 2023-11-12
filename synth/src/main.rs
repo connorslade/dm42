0c38507306bcb6fc46f2522d5f5be7d288d697ca
Connor Slade <connor@connorcode.com>
2023-11-12 17:10:22 -0500

    Cleanup synth

--- synth/src/main.rs
+++ synth/src/main.rs
@@ -1,6 +1,9 @@
 use std::{
     f32::consts::PI,
-    sync::{Arc, Mutex},
+    sync::{
+        atomic::{AtomicU32, Ordering},
+        Arc, Mutex,
+    },
     thread,
 };
 
@@ -15,33 +18,34 @@ fn main() -> Result<()> {
     let port = ports.iter().next().ok_or("No ports found").unwrap();
     let port_name = input.port_name(&port)?;
 
-    println!("Opening connection with `{port_name}`");
+    println!("[*] Opening connection with `{port_name}`");
     let synth = Arc::new(Synth::new());
     let _connection = input.connect(&port, "42synth", on_event, synth.clone())?;
 
     let host = cpal::default_host();
     let device = host.default_output_device().unwrap();
-    println!("Default output device: {}", device.name().unwrap());
+    println!("[*] Output device is `{}`", device.name().unwrap());
 
     let mut supported_configs_range = device.supported_output_configs().unwrap();
     let supported_config = supported_configs_range
         .next()
         .unwrap()
         .with_max_sample_rate();
-    let sample_rate = supported_config.sample_rate().0 as f32;
+    let sample_rate = supported_config.sample_rate().0;
+    synth.update_sample_rate(sample_rate);
+    println!("[*] Sample rate: {}", sample_rate);
 
     let stream = device
         .build_output_stream(
             &supported_config.into(),
             move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
                 let mut voices = synth.voices.lock().unwrap();
-                // dbg!(&voices);
                 for sample in data.iter_mut() {
                     *sample = 0.0;
                     let mut i = 0;
                     while i < voices.len() {
                         let voice = &mut voices[i];
-                        if let Some(s) = voice.oscillator.next(sample_rate) {
+                        if let Some(s) = voice.oscillator.next() {
                             if voice.playing {
                                 *sample += s;
                             }
@@ -50,6 +54,7 @@ fn main() -> Result<()> {
                             voices.remove(i);
                         }
                     }
+                    *sample *= synth.master_volume;
                 }
             },
             move |err| eprintln!("an error occurred on output stream: {}", err),
@@ -65,6 +70,8 @@ fn main() -> Result<()> {
 
 struct Synth {
     voices: Mutex<Vec<Voice>>,
+    master_volume: f32,
+    sample_rate: AtomicU32,
 }
 
 #[derive(Debug)]
@@ -78,6 +85,7 @@ struct Voice {
 struct Oscillator {
     i: usize,
     tone: f32,
+    sample_rate: f32,
     duration: Option<usize>,
 }
 
@@ -101,7 +109,17 @@ impl Synth {
     fn new() -> Self {
         Self {
             voices: Mutex::new(Vec::new()),
+            master_volume: 0.5,
+            sample_rate: AtomicU32::new(0),
         }
+    }
+
+    fn update_sample_rate(&self, sample_rate: u32) {
+        self.sample_rate.store(sample_rate, Ordering::Relaxed);
+    }
+
+    fn sample_rate(&self) -> u32 {
+        self.sample_rate.load(Ordering::Relaxed)
     }
 
     fn note_on(&self, key: u8) {
@@ -127,7 +145,7 @@ impl Synth {
         self.voices.lock().unwrap().push(Voice {
             key,
             playing: true,
-            oscillator: Oscillator::new(note),
+            oscillator: Oscillator::new(note, self.sample_rate()),
         });
     }
 
@@ -141,15 +159,20 @@ impl Synth {
 }
 
 impl Oscillator {
-    fn new(tone: f32) -> Self {
+    fn new(tone: f32, sample_rate: u32) -> Self {
         Self {
-            i: 0,
             tone,
+            i: 0,
+            sample_rate: sample_rate as f32,
             duration: None,
         }
     }
+}
 
-    fn next(&mut self, sample_rate: f32) -> Option<f32> {
+impl Iterator for Oscillator {
+    type Item = f32;
+
+    fn next(&mut self) -> Option<Self::Item> {
         self.i += 1;
 
         match self.duration {
@@ -157,6 +180,6 @@ impl Oscillator {
             _ => {}
         }
 
-        Some((self.i as f32 * self.tone * 2.0 * PI / sample_rate).sin())
+        Some((self.i as f32 * self.tone * 2.0 * PI / self.sample_rate).sin())
     }
 }
